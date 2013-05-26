8de1b0e437c1b325395b8235f2deea3167fddc2d
Robert Qualls <rlqualls@una.edu>
2013-05-25 22:52:43 -0400

    Use have_selector instead of have_content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,13 +8,12 @@ describe "StaticPages" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                                :text => "#{base_title} | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -22,13 +21,12 @@ describe "StaticPages" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-                                :text => "#{base_title} | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -36,12 +34,11 @@ describe "StaticPages" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-                                :text => "#{base_title} | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 end
