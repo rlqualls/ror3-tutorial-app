1befd3f3c9653629c538c13dd1557d45dcb4c266
Robert Qualls <rlqualls@una.edu>
2013-05-27 16:19:44 -0400

    Finish User model with authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -87,5 +87,27 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "with a password that is too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of the authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+
 
 end
