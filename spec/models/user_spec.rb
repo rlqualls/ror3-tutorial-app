0985e91ab8d2afde26cee800cfc544a02a5b4fe2
Robert Qualls <rlqualls@una.edu>
2013-05-27 14:54:58 -0400

    Add email uniqueness validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,5 +28,38 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when email format is invalid" do
+
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_addr|
+        @user.email = invalid_addr
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    
+    it "should be valid" do
+      addresses = %w[user@foo.com A_US-ER@f.b.org first.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_addr|
+        @user.email = valid_addr
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      duplicate_email_user = @user.dup 
+      duplicate_email_user.email = @user.email.upcase
+      duplicate_email_user.save
+    end
+
+    it { should_not be_valid }
+  end
+
 
 end
